a466ba41463df742a54ff937bf9a6a3170d8db11
Nikodem Wójciak <nikodem1703@gmail.com>
2022-08-18 14:00:15 +0200

    Repozytoria Spring Data

--- src/main/java/com/example/demojpa/BikeRepository.java
+++ src/main/java/com/example/demojpa/BikeRepository.java
@@ -1,32 +1,6 @@
 package com.example.demojpa;
 
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-import javax.persistence.EntityManager;
-import java.util.Optional;
-
-@Repository
-public class BikeRepository {
-    private final EntityManager entityManager;
-
-    public BikeRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    public void save(Bike bike) {
-        entityManager.persist(bike);
-    }
-
-    public Optional<Bike> findById(Long id){
-        return Optional.ofNullable(entityManager.find(Bike.class, id));
-    }
-
-    public void deleteById(Long id) {
-        findById(id).ifPresent(entityManager::remove);
-//        Bike bike = entityManager.find(Bike.class, id);
-//        if (bike != null)
-//            entityManager.remove(bike);
-    }
-
-}
+public interface BikeRepository extends CrudRepository<Bike, Long> { }
